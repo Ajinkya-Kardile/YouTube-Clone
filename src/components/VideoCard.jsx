22f3f29c834ae26fe7cbcdc3bf2bda3509f013e2
Ajinkya-Kardile <kardileajinkya@gmail.com>
2024-06-01 16:00:48 +0530

    video card fixes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -12,16 +12,16 @@ import {
 } from '../utils/constants';
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
-  const truncatedTitle = snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60);
+  const truncatedTitle = (snippet?.title || demoVideoTitle).slice(0, 60);
   const channelId = snippet?.channelId || '';
 
   return (
-    <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
+    <Card sx={{ width: { xs: '358px', sm: '358px', md: '310px' }, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={`/video/${videoId || 'cV2gBU6hKfY'}`}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
+          sx={{ width: '100%', height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1E1E1E', height: '106px' }}>
